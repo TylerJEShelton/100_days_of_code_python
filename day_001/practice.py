2b0a46c74e084f9b689c6ac0872353df63a8c535
Tyler <tylershelton99@gmail.com>
2022-03-05 14:39:28 -0500

    Completed Day 1 of the 100 Days of Code: The Complete Python Pro Bootcamp for 2022 Udemy Course

---
+++ day_001/practice.py
@@ -0,0 +1,46 @@
+# print("Hello world!\nHello world!\nHello world!")
+
+# Exercise 1
+print("\nExercise #1:")
+# Write your code below this line 👇
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# print("Hello" + " " + "Tyler")
+
+# Exercise 2
+print("\nExercise #2:")
+#Fix the code below 👇
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+# input() will get the user to input in the console
+# Then print() will print the word "Hello" and the user input
+# print("Hello " + input("What is your name? ") + "!")
+
+# Exercise 3
+print("\nExercise #3:")
+#Write your code below this line 👇
+print(len(input("What is your name? ")))
+
+# name = input("What is your name? ")
+# length = len(name)
+# print("The length of " + name + " is " + str(length) + " characters.")
+
+# Exercise 4
+print("\nExercise #4:")
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+####################################
+#Write your code below this line 👇
+c = a
+a = b
+b = c
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
